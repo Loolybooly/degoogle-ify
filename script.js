c6fe761dae1bb032dcabde18c361eba9ddfeeb0d
Loolybooly <loolytunes@proton.me>
2025-10-24 21:00:08 +0300

    Added requests from some users on discord, some cool features I don't know what to make the commit message for this JUST CHECK OUT THE CHANGES ON THE WEBSITE

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const steps = document.querySelectorAll(".form-step");
+const steps = document.querySelectorAll('.form-step');
 let currentStep = 0;
 
-document
-  .querySelectorAll(".next-btn")
-  .forEach((btn) => btn.addEventListener("click", () => changeStep(1)));
-document
-  .querySelectorAll(".back-btn")
-  .forEach((btn) => btn.addEventListener("click", () => changeStep(-1)));
+document.querySelectorAll('.next-btn').forEach((btn) => {
+  btn.addEventListener('click', () => changeStep(1));
+});
+
+document.querySelectorAll('.back-btn').forEach((btn) => {
+  btn.addEventListener('click', () => changeStep(-1));
+});
 
 function changeStep(dir) {
-  steps[currentStep].classList.remove("active");
+  steps[currentStep].classList.remove('active');
   currentStep = Math.min(Math.max(currentStep + dir, 0), steps.length - 1);
-  steps[currentStep].classList.add("active");
+  steps[currentStep].classList.add('active');
 }
 
-document.getElementById("survey-form").addEventListener("submit", (e) => {
+document.getElementById('survey-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  document.getElementById("survey-form").classList.add("hidden");
-  document.getElementById("results").classList.remove("hidden");
+  document.getElementById('survey-form').classList.add('hidden');
+  document.getElementById('results').classList.remove('hidden');
 });
